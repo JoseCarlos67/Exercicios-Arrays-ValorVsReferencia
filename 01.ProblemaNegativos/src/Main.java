cb92da1179a82a70c57c30fea35f63cce440e215
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-18 15:25:41 -0300

    Primeira atividade concluida

--- 01.ProblemaNegativos/src/Main.java
+++ 01.ProblemaNegativos/src/Main.java
@@ -3,32 +3,42 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        String fakeInput = "4\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
-        ByteArrayInputStream fakeIn = new ByteArrayInputStream(fakeInput.getBytes());
-        System.setIn(fakeIn);
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
 
-        int n = 0;
-        System.out.println("Quantos números você vai digitar? ");
-        n = scanner.nextByte();
-        System.out.println(n);
+      String fakeInput = "13\n10\n1\n-2\n3\n4\n-5\n6\n7\n-8\n9\n10";
+      ByteArrayInputStream fakeIn = new ByteArrayInputStream(fakeInput.getBytes());
+      System.setIn(fakeIn);
+      Scanner scanner = new Scanner(System.in);
 
-        boolean condition = false;
-        while (n > 10 || n < 0) {
-            System.out.println("A quantidade de números de ser maior do que 0 e menor do que 10!");
-            n = scanner.nextByte();
-        }
+      byte n;
+      System.out.println("Quantos números você vai digitar? ");
+      n = scanner.nextByte();
 
-        int vector[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            vector[i] = scanner.nextInt();
-        }
+      while (n > 10 || n < 0) {
+        System.out.println("A quantidade de números de ser maior do que 0 e menor do que 10! Digite novamente: ");
+          n = scanner.nextByte();
+      }
 
-        for (int i = 0; i < n; i++)
-            System.out.println(vector[i]);
+      int[] vector = new int[n];
+      for (byte i = 0; i < n; i++) {
+        vector[i] = scanner.nextInt();
+      }
 
-        scanner.close();
+      for (byte i = 0; i < n; i++)
+        System.out.println(vector[i]);
+
+    System.out.println("\nNÚMEROS NEGATIVOS:");
+    byte count = 0;
+    for (int i = 0; i < n; i++) {
+      if (vector[i] < 0) {
+        System.out.println(vector[i]);
+        count++;
+      }
+    }
+    if (count == 0)
+      System.out.println("NÃO HÁ NÚMEROS NEGATIVOS!");
+
+    scanner.close();
     }
 }
